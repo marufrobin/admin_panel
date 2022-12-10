3c1d1352fa3441c7ddb82b1995009ab26214a352
Maruf Robin <maruf.robin@northsouth.edu>
2022-12-11 00:42:48 +0600

    image, fonts and places fixed added...

--- lib/screen/splash_page.dart
+++ lib/screen/splash_page.dart
@@ -6,8 +6,34 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff3a67d8),
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
         title: Text("Splash page"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Text(
+              "Wellcome to My App",
+              style: TextStyle(
+                color: Color(0xffEAEAEA),
+                fontSize: 36,
+                fontFamily: "Nunito",
+              ),
+            ),
+          ),
+          Expanded(
+              flex: 4,
+              child: Image.asset(
+                "images/r.png",
+                scale: 2,
+              )),
+          Expanded(flex: 2, child: Placeholder())
+        ],
       ),
     );
   }
