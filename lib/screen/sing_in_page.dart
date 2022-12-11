4f8f826b16db76d64d722dd4b37fd779b7203f1f
Maruf Robin <maruf.robin@northsouth.edu>
2022-12-11 09:29:48 +0600

    TextFormField validation work on going..

--- lib/screen/sing_in_page.dart
+++ lib/screen/sing_in_page.dart
@@ -5,6 +5,7 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     TextEditingController emailController = TextEditingController();
     TextEditingController passwordController = TextEditingController();
     return Scaffold(
@@ -17,51 +18,54 @@ class SignInPage extends StatelessWidget {
       ),
       body: Container(
         margin: const EdgeInsets.all(16),
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          TextFormField(
-            controller: emailController,
-            decoration: InputDecoration(
-              hintText: "Email",
-              prefixIcon: Icon(Icons.email_rounded),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(24)),
+        child: Form(
+          key: _formKey,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(
+                hintText: "Email",
+                prefixIcon: Icon(Icons.email_rounded),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(24)),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          TextFormField(
-            controller: passwordController,
-            decoration: InputDecoration(
-              hintText: "Password",
-              prefixIcon: Icon(Icons.password_rounded),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(24)),
+            const SizedBox(
+              height: 16,
             ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Container(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(24))),
-                      elevation: MaterialStateProperty.all(0),
-                      backgroundColor:
-                          MaterialStateProperty.all(const Color(0xff3a67d8))),
-                  onPressed: () {},
-                  child: const Text(
-                    "Sign In",
-                    style: TextStyle(
-                        color: Color(0xffEAEAEA),
-                        fontSize: 16,
-                        fontFamily: "Nunito",
-                        fontWeight: FontWeight.bold),
-                  )))
-        ]),
+            TextFormField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                hintText: "Password",
+                prefixIcon: Icon(Icons.password_rounded),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(24)),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Container(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24))),
+                        elevation: MaterialStateProperty.all(0),
+                        backgroundColor:
+                            MaterialStateProperty.all(const Color(0xff3a67d8))),
+                    onPressed: () {},
+                    child: const Text(
+                      "Sign In",
+                      style: TextStyle(
+                          color: Color(0xffEAEAEA),
+                          fontSize: 16,
+                          fontFamily: "Nunito",
+                          fontWeight: FontWeight.bold),
+                    )))
+          ]),
+        ),
       ),
     );
   }
