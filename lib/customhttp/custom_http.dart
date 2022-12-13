0f2d3d09e0652399343a4ccf46b7dc2986aee76e
Maruf Robin <maruf.robin@northsouth.edu>
2022-12-13 10:42:18 +0600

    custom http work on progress

--- lib/customhttp/custom_http.dart
+++ lib/customhttp/custom_http.dart
@@ -1,8 +1,10 @@
 import 'package:admin_panel/model/order_model.dart';
+import 'package:admin_panel/widget/common_widget.dart';
 
 class CustomHttp {
   static Future<List<OrderModel>> fatchingData() async {
     List<OrderModel> orderList = [];
+    String orderLink = "${baseUrl}";
     return orderList;
   }
 }
