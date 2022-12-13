1294c7d45b99f4b5f632e740afeedada14844ac3
Maruf Robin <maruf.robin@northsouth.edu>
2022-12-13 22:23:14 +0600

    Sign In Page design done...

--- lib/screen/sing_in_page.dart
+++ lib/screen/sing_in_page.dart
@@ -62,103 +62,140 @@ class _SignInPageState extends State<SignInPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       // backgroundColor: Color(0xff3a67d8),
-      appBar: AppBar(
-        backgroundColor: Colors.cyan,
-        centerTitle: true,
-        elevation: 0,
-        title: const Text(
-          "Sign In Page",
-          style: TextStyle(
-              color: Color(0xffEAEAEA),
-              fontSize: 20,
-              fontFamily: "Nunito",
-              fontWeight: FontWeight.bold),
-        ),
-      ),
-      body: Container(
-        margin: const EdgeInsets.all(16),
-        child: Form(
-          key: _formKey,
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            TextFormField(
-              controller: emailController,
-              validator: (email) {
-                if (widget.isEmailValid(email!)) {
-                  return null;
-                } else
-                  return 'Enter a valid email address';
-              },
-              decoration: InputDecoration(
-                hintText: "Email",
-                labelText: "Email",
-                prefixIcon: Icon(Icons.email_rounded),
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(24)),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.cyan,
+      //   centerTitle: true,
+      //   elevation: 0,
+      //   title: const Text(
+      //     "Sign In Page",
+      //     style: TextStyle(
+      //         color: Color(0xffEAEAEA),
+      //         fontSize: 20,
+      //         fontFamily: "Nunito",
+      //         fontWeight: FontWeight.bold),
+      //   ),
+      // ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: -360,
+            left: -100,
+            child: CircleAvatar(
+              backgroundColor: Color(0xff1C58C9),
+              radius: 300,
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.width * 0.2,
+            left: MediaQuery.of(context).size.width * 0.34,
+            child: Image.asset(
+              "images/r.png",
+              scale: 4,
+            ),
+          ),
+          SafeArea(
+            child: Container(
+              margin: const EdgeInsets.all(16),
+              child: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                      // crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.30,
+                        ),
+                        // Spacer(),
+                        // Expanded(
+                        //     child: SizedBox(
+                        //   height: MediaQuery.of(context).size.height * 0.4,
+                        // )),
+                        TextFormField(
+                          controller: emailController,
+                          validator: (email) {
+                            if (widget.isEmailValid(email!)) {
+                              return null;
+                            } else
+                              return 'Enter a valid email address';
+                          },
+                          decoration: InputDecoration(
+                            hintText: "Email",
+                            labelText: "Email",
+                            prefixIcon: Icon(Icons.email_rounded),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(24)),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        TextFormField(
+                          controller: passwordController,
+                          obscureText: isObscureText,
+                          validator: (password) {
+                            if (widget.isPasswordValid(password!))
+                              return null;
+                            else
+                              return 'Enter a valid password';
+                          },
+                          decoration: InputDecoration(
+                              hintText: "Password",
+                              labelText: "Password",
+                              prefixIcon: Icon(Icons.password_rounded),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(24)),
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    isObscureText = !isObscureText;
+                                    setState(() {});
+                                  });
+                                },
+                                icon: isObscureText == true
+                                    ? Icon(Icons.visibility_rounded)
+                                    : Icon(Icons.visibility_off_rounded),
+                              )),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        Container(
+                            width: double.infinity,
+                            height: 50,
+                            child: ElevatedButton(
+                                style: ButtonStyle(
+                                    shape: MaterialStateProperty.all(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(24))),
+                                    elevation: MaterialStateProperty.all(0),
+                                    backgroundColor: MaterialStateProperty.all(
+                                        const Color(0xff3a67d8))),
+                                onPressed: () {
+                                  // if (_formKey.currentState.validate()) {
+                                  //   _formKey.currentState.save();
+                                  //   // use the email provided here
+                                  // }
+                                  if (_formKey.currentState!.validate()) {
+                                    _formKey.currentState!.save();
+                                    facthingTokenData();
+                                  }
+                                },
+                                child: const Text(
+                                  "Sign In",
+                                  style: TextStyle(
+                                      color: Color(0xffEAEAEA),
+                                      fontSize: 16,
+                                      fontFamily: "Nunito",
+                                      fontWeight: FontWeight.bold),
+                                ))),
+                        // Expanded(child: SizedBox()),
+                      ]),
+                ),
               ),
             ),
-            const SizedBox(
-              height: 16,
-            ),
-            TextFormField(
-              controller: passwordController,
-              obscureText: isObscureText,
-              validator: (password) {
-                if (widget.isPasswordValid(password!))
-                  return null;
-                else
-                  return 'Enter a valid password';
-              },
-              decoration: InputDecoration(
-                  hintText: "Password",
-                  labelText: "Password",
-                  prefixIcon: Icon(Icons.password_rounded),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(24)),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        isObscureText = !isObscureText;
-                        setState(() {});
-                      });
-                    },
-                    icon: isObscureText == true
-                        ? Icon(Icons.visibility_rounded)
-                        : Icon(Icons.visibility_off_rounded),
-                  )),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Container(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                    style: ButtonStyle(
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(24))),
-                        elevation: MaterialStateProperty.all(0),
-                        backgroundColor:
-                            MaterialStateProperty.all(const Color(0xff3a67d8))),
-                    onPressed: () {
-                      // if (_formKey.currentState.validate()) {
-                      //   _formKey.currentState.save();
-                      //   // use the email provided here
-                      // }
-                      if (_formKey.currentState!.validate()) {
-                        _formKey.currentState!.save();
-                        facthingTokenData();
-                      }
-                    },
-                    child: const Text(
-                      "Sign In",
-                      style: TextStyle(
-                          color: Color(0xffEAEAEA),
-                          fontSize: 16,
-                          fontFamily: "Nunito",
-                          fontWeight: FontWeight.bold),
-                    )))
-          ]),
-        ),
+          ),
+        ],
       ),
     );
   }
